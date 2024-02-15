b326260aa0543813ab769238ebf2c00b918767cc
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2024-02-15 19:59:09 +0100

    Adding the implementation into the main method.
    This is for testing proposals

--- Program.cs
+++ Program.cs
@@ -4,10 +4,40 @@ namespace aws
     using aws.utils.dynamodb;
     class Program
     {
+        /// <summary>
+        /// In the main program method, the application is calling all the functionalities 
+        /// related with the aws dotnet tool service.
+        /// </summary>
         static void Main(string[] args)
         {
+            // If the dynamodb table does not exist this piece of code will create it
+            // If the dynamodb table exists, the code will fail.
+            // DynamoDBBuilder builder = new DynamoDBBuilder("TestTable2");
+            // builder.AddSchema("pk", "HASH")
+            //     .AddSchema("sk", "RANGE")
+            //     .AddDefinition("pk", "S")
+            //     .AddDefinition("sk", "S")
+            //     .AddDefinition("Name", "S")
+            //     .AddDefinition("LastName", "S")
+            //     .SetProvisionedThroughput(1,1)
+            //     .AddSecondaryIndex("TestIndex", "Name", "RANGE", "ALL")
+            //     .AddSecondaryIndex("TestIndex2", "LastName", "RANGE", "ALL")
+            //     .CreateTable();
+
+            // If the sk is not in the dynamodb table, this piece of code will create a new record
+            // if it exists it will update the previous record
+            Console.WriteLine("Start Add/Update Item!");
+            DynamoDBCommandBuilder build = new DynamoDBCommandBuilder("TestTable2")
+                .AddAttribute("pk", "Id")
+                .AddAttribute("sk", "3")
+                .AddAttribute("Name", "Anne")
+                .AddAttribute("LastName", "Rolo");
+
+            build.AddUpdateItem();
+            Console.WriteLine("End Add/Update Item!");
+
             Console.WriteLine("Start Query!");
-            DynamoDBBuilder queryBuilder = new DynamoDBBuilder("TestTable")
+            DynamoDBCommandBuilder queryBuilder = new DynamoDBCommandBuilder("TestTable")
                 .AddKeyCondition("pk", "User")
                 .MustEqualFilter("Name", "Pedro")
                 .MustEqualFilter("LastName", "Pacheco")
@@ -25,7 +55,7 @@ namespace aws
             Console.WriteLine("End Query!");
 
             Console.WriteLine("Start Scan!");
-            DynamoDBBuilder scanBuilder = new DynamoDBBuilder("TestTable")
+            DynamoDBCommandBuilder scanBuilder = new DynamoDBCommandBuilder("TestTable")
                 .MustEqualFilter("Name", "Pedro")
                 .MustEqualFilter("LastName", "Pacheco")
                 .CouldEqualFilter("Age", "26");
